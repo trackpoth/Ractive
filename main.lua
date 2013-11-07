9f400e4b3ecfa13998c2f28a5389ff826b3679fd
darkstalker <slayerbeast@gmail.com>
2013-11-07 16:14:30 -0300

    code style improvements and cleanups

--- main.lua
+++ main.lua
@@ -1,98 +1,102 @@
-require "player/player"
-require "SpriteAnimation"
-require "camera"
+local Player = require "player/player"
+local SpriteAnimation = require "SpriteAnimation"
+local Camera = require "camera"
 
-function love.load()	
+local g, loader, map, camera, animation, p, gravity, delay, hasJumped
+
+function love.load()
     g = love.graphics
     width = g.getWidth()
     height = g.getHeight()
     g.setBackgroundColor(0, 137, 255)
-	
-	loader = require("AdvTiledLoader.Loader")
+
+    loader = require("AdvTiledLoader.Loader")
     loader.path = "maps/"
+
     map = loader.load("map01.tmx")
     map:setDrawRange(0, 0, map.width * map.tileWidth, map.height * map.tileHeight)
-	camera:setBounds(0, 0, map.width * map.tileWidth - width, map.height * map.tileHeight - height)
-	
-	animation = SpriteAnimation:new("player/playersprites.png", 24, 32, 4, 4)
+
+    camera = Camera:new()
+    camera:setBounds(0, 0, map.width * map.tileWidth - width, map.height * map.tileHeight - height)
+
+    animation = SpriteAnimation:new("player/playersprites.png", 24, 32, 4, 4)
     animation:load(delay)
-    
-	
+
     p = Player:new()
-    
+
     p.x = 300
     p.y = 300
     p.width = 24
     p.height = 32
     p.jumpSpeed = -420
     p.runSpeed = 180
-    
+
     gravity = 1000
     delay = 120
     hasJumped = false
 end
- 
+
 function love.update(dt)
-	if (dt > 1/60) then 
-		dt = 1/60 
-	end
-	
+    if dt > 1/60 then
+        dt = 1/60
+    end
+
     if love.keyboard.isDown("left") then
         p:moveLeft()
-		animation:flip(true, false)
+        animation:flip(true, false)
     end
     if love.keyboard.isDown("right") then
         p:moveRight()
-		animation:flip(false, false)
+        animation:flip(false, false)
     end
     if love.keyboard.isDown("x") then
         p:jump()
     end
 
     p:update(dt, gravity, map)
-	
-    if (p.state == "stand") then
+
+    if p.state == "stand" then
         animation:switch(1, 2, 1000)
     end
-    if (p.state == "moveRight") or (p.state == "moveLeft") then
+    if p.state == "moveRight" or p.state == "moveLeft" then
         animation:switch(2, 2, 120)
     end
-    if (p.state == "jump") then
+    if p.state == "jump" then
         animation:switch(3, 1, 300)
     end
-    if (p.state == "fall") then
+    if p.state == "fall" then
         animation:switch(4, 1, 300)
     end
     animation:update(dt)
-	camera:setPosition(math.floor(p.x - width / 2), math.floor(p.y - height / 2))
+    camera:setPosition(math.floor(p.x - width / 2), math.floor(p.y - height / 2))
 end
- 
+
 function love.draw()
     local x = math.floor(p.x)
     local y = math.floor(p.y)
-	local camX, camY = camera._x, camera._y
+    local camX, camY = camera._x, camera._y
     local tileX = math.floor(p.x / map.tileWidth)
     local tileY = math.floor(p.y / map.tileHeight)
-    
-	camera:set()
-	
-	map:draw()
-	
+
+    camera:set()
+
+    map:draw()
+
     animation:draw(x - p.width / 2, y - p.height / 2)
-	
+
     camera:unset()
-	
-	g.setColor(255, 255, 255)
+
+    g.setColor(255, 255, 255)
     g.print("Player coordinates: ("..x..","..y..")", 5, 5)
     g.print("Current state: "..p.state, 5, 20)
-	love.graphics.print("Current tile: ("..tileX..", "..tileY..")", 5, 35)
+    love.graphics.print("Current tile: ("..tileX..", "..tileY..")", 5, 35)
 end
- 
+
 function love.keyreleased(key)
     if key == "escape" then
         love.event.push("quit")
     end
-    if (key == "right") or (key == "left") then
+    if key == "right" or key == "left" then
         p:stop()
     end
 end
--- player/player.lua
+++ player/player.lua
@@ -1,43 +1,42 @@
-Player = {}
+local Player = {}
 
 function Player:new()
     local object = {
-    x = 0,
-    y = 0,
-    width = 0,
-    height = 0,
-    xSpeed = 0,
-    ySpeed = 0,
-    xSpeedMax = 800,
-    ySpeedMax = 800,
-    state = "",
-    jumpSpeed = 0,
-    runSpeed = 0,
-    onFloor = false
+        x = 0,
+        y = 0,
+        width = 0,
+        height = 0,
+        xSpeed = 0,
+        ySpeed = 0,
+        xSpeedMax = 800,
+        ySpeedMax = 800,
+        state = "",
+        jumpSpeed = 0,
+        runSpeed = 0,
+        onFloor = false,
     }
-    setmetatable(object, { __index = Player })
-    return object
+    return setmetatable(object, { __index = self })
 end
- 
+
 function Player:jump()
     if self.onFloor then
         self.ySpeed = self.jumpSpeed
         self.onFloor = false
     end
 end
- 
+
 function Player:moveRight()
     self.xSpeed = self.runSpeed
 end
- 
+
 function Player:moveLeft()
     self.xSpeed = -self.runSpeed
 end
- 
+
 function Player:stop()
     self.xSpeed = 0
 end
- 
+
 function Player:collide(event)
     if event == "floor" then
         self.ySpeed = 0
@@ -47,19 +46,19 @@ function Player:collide(event)
         self.ySpeed = 0
     end
 end
- 
+
 function Player:update(dt, gravity, map)
     local halfX = self.width / 2
     local halfY = self.height / 2
-   
+
     self.ySpeed = self.ySpeed + (gravity * dt)
-   
+
     self.ySpeed = math.clamp(self.ySpeed, -self.ySpeedMax, self.ySpeedMax)
-	
+
     local nextY = math.floor(self.y + (self.ySpeed * dt))
     if self.ySpeed < 0 then
-        if not(self:isColliding(map, self.x - halfX + 1, nextY - halfY))
-            and not(self:isColliding(map, self.x + halfX - 1, nextY - halfY)) then
+        if not self:isColliding(map, self.x - halfX + 1, nextY - halfY)
+            and not self:isColliding(map, self.x + halfX - 1, nextY - halfY) then
             self.y = nextY
             self.onFloor = false
         else
@@ -67,8 +66,8 @@ function Player:update(dt, gravity, map)
             self:collide("ceiling")
         end
     elseif self.ySpeed > 0 then
-        if not(self:isColliding(map, self.x - halfX + 1, nextY + halfY))
-            and not(self:isColliding(map, self.x + halfX - 1, nextY + halfY)) then
+        if not self:isColliding(map, self.x - halfX + 1, nextY + halfY)
+            and not self:isColliding(map, self.x + halfX - 1, nextY + halfY) then
             self.y = nextY
             self.onFloor = false
         else
@@ -76,26 +75,26 @@ function Player:update(dt, gravity, map)
             self:collide("floor")
         end
     end
-	
+
     local nextX = self.x + (self.xSpeed * dt)
     if self.xSpeed > 0 then
-        if not(self:isColliding(map, nextX + halfX, self.y - halfY))
-            and not(self:isColliding(map, nextX + halfX, self.y + halfY - 1)) then
+        if not self:isColliding(map, nextX + halfX, self.y - halfY)
+            and not self:isColliding(map, nextX + halfX, self.y + halfY - 1) then
             self.x = nextX
         else
             self.x = nextX - ((nextX + halfX) % map.tileWidth)
         end
     elseif self.xSpeed < 0 then
-        if not(self:isColliding(map, nextX - halfX, self.y - halfY))
-            and not(self:isColliding(map, nextX - halfX, self.y + halfY - 1)) then
+        if not self:isColliding(map, nextX - halfX, self.y - halfY)
+            and not self:isColliding(map, nextX - halfX, self.y + halfY - 1) then
             self.x = nextX
         else
             self.x = nextX + map.tileWidth - ((nextX - halfX) % map.tileWidth)
         end
     end
-	
+
 	self.state = self:getState()
-	
+
 end
 
 function Player:isColliding(map, x, y)
@@ -125,3 +124,5 @@ function Player:getState()
     end
     return myState
 end
+
+return Player
